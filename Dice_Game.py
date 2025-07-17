f42e2494a19edcd4f12ffacefa4b7d32d5fdcda9
kvshivaprasad <kvshivaprasad816@gmail.com>
2025-07-17 17:21:30 +0530

    add readme

--- Dice_Game.py
+++ Dice_Game.py
@@ -1,14 +1,14 @@
-import random 
+import random  # Importing random to guess the any number 
 
-while True:
- dice = input("roll the dice (y/n): ").lower()   
- if dice == "y":
-    dice1 = random.randint(1,6)
-    dice2 = random.randint(1,6)
+while True: # To loop inifinite 
+ dice = input("Roll the dice (y/n): ").lower()   # Ask the user to play this game simply yes or no
+ if dice == "y": # condition checking 
+    dice1 = random.randint(1,6) 
+    dice2 = random.randint(1,6) # choose to dice to play the game 
     print(f"{dice1} and {dice2}")
- elif dice == "n":
+ elif dice == "n": # Stop the game
     print("Thank you!")
-    break
+    break # Break the loop 
  else:
-    print("Invalid option")
+    print("Invalid option") 
   
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Dice_Rolling_Game
+
 A simple and fun dice game built with python
+
+<h1> DICE ROLLING GAME </h1>
+<hr>
+<p>This is the simple dice game to guess the dice number creating using python inside the code importing random module then  use while loop, True - infinite loop on the game when user want to exit the loop break </p>
+To play this game copy the code and past it on vs code or any code editor.
+1.Open Terminal
+2.Type yes or no (y/n) - To play this game choose one option 
+3.if you choose y then computer generate the two random number 
+4.if you choose n then thank you message printing 
+5.remenber only choose (y,n), Other can be
